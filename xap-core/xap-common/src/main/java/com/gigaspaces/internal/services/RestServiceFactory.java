d8de1e0817b5e85da2db055b209d1de57e54043d
evgenyf <evgenyf>
2019-06-16 17:45:11 +0300

    GS-13625 added hsqldb jar to rest service classpath

--- xap-core/xap-common/src/main/java/com/gigaspaces/internal/services/RestServiceFactory.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/internal/services/RestServiceFactory.java
@@ -45,7 +45,8 @@ public class RestServiceFactory extends ServiceFactory {
                 .appendOptional("jetty")
                 .appendOptional("jetty/xap-jetty")
                 .appendOptional("jackson")
-                .appendOptional("metrics");
+                .appendOptional("metrics")
+                .appendOptional("jdbc");
 
     }
 }
